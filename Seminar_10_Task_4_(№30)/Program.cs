205280ca800fa8e7aea24e386b7c59c0c07ac066
Aleks37 <lao35180972643335589007>
2023-10-01 15:12:16 +0400

    решение через цикл foreach

--- Seminar_10_Task_4_(№30)/Program.cs
+++ Seminar_10_Task_4_(№30)/Program.cs
@@ -2,13 +2,19 @@
 // заполненный нулями и 1-ми в случайном порядке [1,0,1,1,0,1,0,0].
 int[]array = new int[8];
 
-void fillArray(int[] array){
-int index = 0;
-while (index < array.Length)
+void fillArray(int[] array)
+{
+
+    for (int i=0; i < array.Length; i++)   // - решение через цикл for
     {
-    array[index] = new Random().Next(2); // Next(0, 2) - равноценно
-    Console.Write(array[index] + " ");
-    index++;
+        array[i] = new Random().Next(2); // Next(0, 2) - равноценно
+        Console.Write(array[i] + " ");
+    }
+
+    foreach (int element in array)   // - решение через цикл foreach
+    {
+        array[element] = new Random().Next(2); 
+        Console.Write(array[element] + " ");
     }
 }
 fillArray(array);
