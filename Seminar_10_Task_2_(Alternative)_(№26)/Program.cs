0819d315df3bb52374d6d135477278016a6f7384
Aleks37 <lao35180972643335589007>
2023-10-01 12:26:19 +0400

    Add Seminar_10_Task_2_(Alternative)_(№26)

---
+++ Seminar_10_Task_2_(Alternative)_(№26)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Альтерантивное решение задачи по подсчёту размерности числа (ч/з строку).
+
+int Print (string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+static int DigitsNumber (int value)
+{
+    if (value < 0)
+    {
+        value = -value;
+    }
+    string number = value.ToString();
+    return number.Length;
+}
+int number = Print("Введите число:  ");
+Console.Write($"Кол-во цифр в данном числе:  {DigitsNumber(number)}");
